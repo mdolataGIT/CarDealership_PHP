90c99f658b00e3293d13306c2f3136bd7b5b4f82
macie <macie@DESKTOP-K6OP92M>
2020-05-27 21:26:15 +0200

    1

---
+++ public/templates_c/143aabf86739acb1e26c3a2015133db4924fb52c_0.file.Hello.tpl.php
@@ -0,0 +1,56 @@
+<?php
+/* Smarty version 3.1.34-dev-7, created on 2020-05-27 21:22:15
+  from 'D:\xpp\Nowy folder\htdocs\Projekt_Maciej_Dolata_komis\app\views\Hello.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.34-dev-7',
+  'unifunc' => 'content_5ecebde79250d5_65856672',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '143aabf86739acb1e26c3a2015133db4924fb52c' => 
+    array (
+      0 => 'D:\\xpp\\Nowy folder\\htdocs\\Projekt_Maciej_Dolata_komis\\app\\views\\Hello.tpl',
+      1 => 1524337902,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5ecebde79250d5_65856672 (Smarty_Internal_Template $_smarty_tpl) {
+?><!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+
+<head>
+	<meta charset="utf-8"/>
+	<title>Hello World | Amelia framework</title>
+</head>
+
+<body>
+    
+    My value: <?php echo $_smarty_tpl->tpl_vars['value']->value;?>
+
+    
+    <?php if ($_smarty_tpl->tpl_vars['msgs']->value->isInfo()) {?>
+        <ul>
+        <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['msgs']->value->getMessages(), 'msg');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['msg']->value) {
+?>
+            <li><?php echo $_smarty_tpl->tpl_vars['msg']->value->text;?>
+</li>
+        <?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+        </ul>
+    <?php }?>
+
+</body>
+
+</html><?php }
+}
