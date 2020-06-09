979d660d16c4dde738a07971763d15b32d01c299
macie <macie@DESKTOP-K6OP92M>
2020-06-09 03:27:52 +0200

    all tables without coustomer and url problems + no com

--- app/controllers/CompanyEditCtrl.class.php
+++ app/controllers/CompanyEditCtrl.class.php
@@ -10,16 +10,13 @@ use app\forms\CompanyEditForm;
 
 class CompanyEditCtrl {
 
-    private $form; //dane formularza
+    private $form;
 
     public function __construct() {
-        //stworzenie potrzebnych obiektów
         $this->form = new CompanyEditForm();
     }
 
-    // Walidacja danych przed zapisem (nowe dane lub edycja).
     public function validateSave() {
-        //0. Pobranie parametrów z walidacją
         $this->form->id = ParamUtils::getFromRequest('id', true, 'Błędne wywołanie aplikacji');
         $this->form->nazwa = ParamUtils::getFromRequest('nazwa', true, 'Błędne wywołanie aplikacji');
         $this->form->miejscowosc = ParamUtils::getFromRequest('miejscowosc', true, 'Błędne wywołanie aplikacji');
@@ -29,7 +26,6 @@ class CompanyEditCtrl {
         if (App::getMessages()->isError())
             return false;
 
-        // 1. sprawdzenie czy wartości wymagane nie są puste
         if (empty(trim($this->form->nazwa))) {
             Utils::addErrorMessage('Wprowadź nazwe');
         }
@@ -52,10 +48,7 @@ class CompanyEditCtrl {
         return !App::getMessages()->isError();
     }
 
-    //validacja danych przed wyswietleniem do edycji
     public function validateEdit() {
-        //pobierz parametry na potrzeby wyswietlenia danych do edycji
-        //z widoku listy osób (parametr jest wymagany)
         $this->form->id = ParamUtils::getFromCleanURL(1, true, 'Błędne wywołanie aplikacji');
         return !App::getMessages()->isError();
     }
@@ -63,17 +56,14 @@ class CompanyEditCtrl {
     public function action_companyNew() {
         $this->generateView();
     }
-
-    //wysiweltenie rekordu do edycji wskazanego parametrem 'id'
+    
     public function action_companyEdit() {
-        // 1. walidacja id osoby do edycji
+
         if ($this->validateEdit()) {
             try {
-                // 2. odczyt z bazy danych osoby o podanym ID (tylko jednego rekordu)
                 $record = App::getDB()->get("firma", "*", [
                     "idfirma" => $this->form->id
                 ]);
-                // 2.1 jeśli osoba istnieje to wpisz dane do obiektu formularza
                 $this->form->id = $record['idfirma'];
                 $this->form->nazwa = $record['nazwa'];
                 $this->form->miejscowosc = $record['miejscowosc'];
@@ -85,17 +75,13 @@ class CompanyEditCtrl {
                     Utils::addErrorMessage($e->getMessage());
             }
         }
-
-        // 3. Wygenerowanie widoku
         $this->generateView();
     }
 
     public function action_companyDelete() {
-        // 1. walidacja id osoby do usuniecia
         if ($this->validateEdit()) {
 
             try {
-                // 2. usunięcie rekordu
                 App::getDB()->delete("firma", [
                     "idfirma" => $this->form->id
                 ]);
@@ -107,18 +93,14 @@ class CompanyEditCtrl {
             }
         }
 
-        // 3. Przekierowanie na stronę listy osób
         App::getRouter()->forwardTo('companyList');
     }
 
     public function action_companySave() {
 
-        // 1. Walidacja danych formularza (z pobraniem)
         if ($this->validateSave()) {
-            // 2. Zapis danych w bazie
             try {
 
-                //2.1 Nowy rekord
                 if ($this->form->id == '') {              
                         App::getDB()->insert("firma", [
                             "nazwa" => $this->form->nazwa,
@@ -127,7 +109,6 @@ class CompanyEditCtrl {
                             "arch" => $this->form->arch
                         ]);                 
                 } else {
-                    //2.2 Edycja rekordu o danym ID
                     App::getDB()->update("firma", [
                         "nazwa" => $this->form->nazwa,
                         "miejscowosc" => $this->form->miejscowosc,
@@ -143,17 +124,14 @@ class CompanyEditCtrl {
                 if (App::getConf()->debug)
                     Utils::addErrorMessage($e->getMessage());
             }
-
-            // 3b. Po zapisie przejdź na stronę listy osób (w ramach tego samego żądania http)
             App::getRouter()->forwardTo('companyList');
         } else {
-            // 3c. Gdy błąd walidacji to pozostań na stronie
             $this->generateView();
         }
     }
 
     public function generateView() {
-        App::getSmarty()->assign('form', $this->form); // dane formularza dla widoku
+        App::getSmarty()->assign('form', $this->form);
         App::getSmarty()->display('CompanyEdit.tpl');
     }
 
